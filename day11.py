286cee4bcf2b067da55e33df9d1156862d06bdbc
Omar Elsherif <omar343@gmail.com>
2025-12-11 21:03:48 -0500

    submit day 11

--- day11.py
+++ day11.py
@@ -1,5 +1,4 @@
-import time
-from collections import defaultdict
+import time, numpy as np
 t0 = time.time()
 with open("inputs/11.txt", "r") as File:
     lines = [line[:-1] for line in File.readlines()]
@@ -9,21 +8,35 @@ for line in lines:
     before, after = line.split(":")
     outputs[before] = after.split()
 
-start = 'you'
-current = ['you']
-ans_p1 = 0
-while current:
-    new = []
-    for dev1 in current:
-        conns = outputs[dev1]
-        for conn in conns:
-            if conn == 'out':
-                ans_p1 += 1
-            else:
-                new.append(conn)
-    current = new.copy()
+def count_paths(start: str, ends: list[str], memory):
+    past_ans = memory.get(start)
+    if past_ans is not None:
+        return past_ans
+    if start in ends:
+        ans = np.zeros(len(ends), dtype = np.int64)
+        idx = ends.index(start)
+        ans[idx] = 1
+        return ans
+    conns = outputs[start]
+    ans = sum(count_paths(conn, ends, memory) for conn in conns)
+    memory[start] = ans
+    return ans
+    
+def paths_from_to(start: str, ends: list[str]):
+    '''setting up memoization'''
+    memory = {}
+    return count_paths(start, ends, memory)
+
+ans_p1 = paths_from_to('you', ['out'])[0]
+
+svr_to_dac, svr_to_fft, svr_to_out = paths_from_to('svr', ['dac', 'fft', 'out'])
+dac_to_fft, dac_to_out = paths_from_to('dac', ['fft', 'out'])
+fft_to_dac, fft_to_out = paths_from_to('fft', ['dac', 'out'])
+
+ans_p2 = svr_to_dac * dac_to_fft * fft_to_out + svr_to_fft * fft_to_dac * dac_to_out
 
 t1 = time.time()
+
 print(f"Part 1 answer: {ans_p1}")
-#print(f"Part 2 answer: {ans_p2}")
+print(f"Part 2 answer: {ans_p2}")
 print(f"Time: {t1 - t0:.3f}s")
